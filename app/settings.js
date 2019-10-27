db7e088a8db69344f218d2433bc7613cd6e0cbd7
Ian Savchenko <ian@Ians-MBP.lan>
2019-10-27 23:07:45 +0100

    start week always from Monday according ISO Week Number

--- app/settings.js
+++ app/settings.js
@@ -1,25 +1,26 @@
-import {preferences} from 'user-settings';
-import {readAll} from './settings-storage';
+// import {preferences} from 'user-settings';
+// import {readAll} from './settings-storage';
 
 export const MAX_WEEK_DIFF = 52;
 
 export let getFirstDayOfWeek = function() {
-  let all = readAll();
-  if (!all || !all.firstDayOfWeek || !all.firstDayOfWeek.values) {
-    return preferences.firstDayOfWeek;
-  }
+  // let all = readAll();
+  // if (!all || !all.firstDayOfWeek || !all.firstDayOfWeek.values) {
+  //   return preferences.firstDayOfWeek;
+  // }
 
-  let value = all.firstDayOfWeek.values[0].value;
-  switch(value) {
-  case('auto'):
-    return preferences.firstDayOfWeek;
-  case('sunday'):
-    return 0;
-  case('monday'):
-    return 1;
-  default: 
-    return preferences.firstDayOfWeek;
-  }
+  // let value = all.firstDayOfWeek.values[0].value;
+  // switch(value) {
+  // case('auto'):
+  //   return preferences.firstDayOfWeek;
+  // case('sunday'):
+  //   return 0;
+  // case('monday'):
+  //   return 1;
+  // default: 
+  //   return preferences.firstDayOfWeek;
+  // }
+  return 1; // ISO Week Number always starts on Monday
 };
 
 export default {
